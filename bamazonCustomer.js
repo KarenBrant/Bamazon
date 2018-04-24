eecd802de4c3ef5429c6718cde6d44dbcd799cf8
Karen Brant <karen.brant3@gmail.com>
2018-04-23 21:21:24 -0400

    everything works except update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -52,64 +52,112 @@ var prodID = 0;
 var getAmt = 0;
 var getID = 0;
 
-function idSearch() {
-        inquirer
-          .prompt({
-            name: "idNum",
-            type: "input",
-            message: "What product (by id) do you want to buy?"
-          })
-          .then(function(answer) {
-            var query = "SELECT id, product_name, stock_quantity FROM products WHERE ?";
-            connection.query(query, { id: answer.idNum }, function(err, res) {
-                if (err) throw err;
-                for (var i = 0; i < res.length; i++) {
-                    console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Quantity: " + res[i].stock_quantity);
-                }
-            prodID = res.id;
-            getAmount();
-            });
-          });
-      }
+// function idSearch() {
+//         inquirer
+//           .prompt({
+//             name: "idNum",
+//             type: "input",
+//             message: "What product (by id) do you want to buy?"
+//           })
+//           .then(function(answer) {
+//             var query = "SELECT id, product_name, stock_quantity FROM products WHERE ?";
+//             connection.query(query, { id: answer.idNum }, function(err, res) {
+//                 if (err) throw err;
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Quantity: " + res[i].stock_quantity);
+//                 }
+//             prodID = res.id;
+//             getAmount(prodID);
+//             });
+//           });
+//       }
 
     //   idSearch();
 
 
-    function getAmount() {
-            inquirer
-          .prompt({
-            name: "quant",
-            type: "input",
-            message: "How many would you like to buy?"
-          })
-          .then(function(answer) {
-            // Log all results of the SELECT statement
-            var query = "SELECT id, product_name, stock_quantity FROM products WHERE ?";
-            connection.query(query, { stock_quantity: answer.quant }, function(err, results) {
-                if (err) throw err;
-                console.log("amt: " + amt);
-                for (var i = 0; i < results.length; i++) {
-                    console.log("prodID: " + prodID);
-                }    
-                if (amt > results.stock_quantity) {
-                    console.log ("Insufficent quantity");
-                } else {
-                    console.log("ID: " + results[i].id + " || Product Name: " + results[i].product_name + " || Amount to purchase: " + results[i].stock_quantity);
-                }
-                
-                amt = answer.quant;
-                confirmOrder();
-            });  
-        }); 
-        // console.log ("amt: " + amt);
-        // console.log ("id: " + prodID);
-        return amt, prodID;
+    // function getAmount(idNum) {
+    //         inquirer
+    //       .prompt({
+    //         name: "quant",
+    //         type: "input",
+    //         message: "How many would you like to buy?"
+    //       })
+    //       .then(function(answer) {
+    //         // Log all results of the SELECT statement
+    //         amt = answer.quant;
+    //         var query = "SELECT id, product_name, stock_quantity FROM products WHERE ?";
+    //         connection.query(query, { id: idNum }, function(err, results) {
+    //             if (err) throw err;
+    //             console.log("amt: " + amt);
+    //             // for (var i = 0; i < results.length; i++) {
+                    
+    //             // }    
+    //             if (amt > results.stock_quantity) {
+    //                 console.log ("Insufficent quantity");
+    //             } else {
+    //                 for (var i = 0; i < results.length; i++) {
+    //                     console.log("ID: " + results[i].id + " || Product Name: " + results[i].product_name + " || Amount to purchase: " + results[i].stock_quantity);
+    //                 }
+    //             amt -= results.stock_quantity;
+    //             }
+    //             console.log("amt in getAmount: " + amt);
+    //             console.log("prodID in getAmount: " + prodID);
+    //             confirmOrder(amt, idNum);
+    //         });  
+    //     }); 
+    //     // console.log ("amt: " + amt);
+    //     // console.log ("id: " + prodID);
+    //     // return amt, prodID;
         
-    }
+    // }
 
+    function idSearch() {
+        inquirer
+          .prompt([
+            {
+              name: "idNum",
+              type: "input",
+              message: "What product (by id) do you want to buy?",
+              validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+            },
+            {
+              name: "quant",
+              type: "input",
+              message: "How many would you like to buy?",
+              validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+            }
+          ])
+          .then(function(answer) {
+            var query = "SELECT id,product_name, stock_quantity FROM products WHERE ?";
+            connection.query(query, {id: answer.idNum}, function(err, res) {
+              for (var i = 0; i < res.length; i++) {
+                console.log(
+                  "ID: " +
+                    res[i].id +
+                    " || Product name: " +
+                    res[i].product_name +
+                    " || Amount to buy: " +
+                    answer.quant
+                );
+              }
+              confirmOrder();
+            });
+          });
+      }
 
-
-    function confirmOrder(amt, prodID) {
+    function confirmOrder() {
+        // console.log("amt in updateProd: " + amt);
+        // console.log("prodID in updateProd: " + prodID);
 
         inquirer.prompt([
             {
@@ -123,7 +171,9 @@ function idSearch() {
                 if (order.orderPlace == false) {
                     return false;
                 } else {
-                    updateProd(amt, prodID);
+                    // console.log("amt in updateProd: " + amt);
+                    // console.log("prodID in updateProd: " + prodID);
+                    updateProd();
                 }
         });
     }
@@ -136,7 +186,7 @@ function updateProd(amt, prodID) {
     var query = connection.query(
       "UPDATE products SET ? WHERE ?",
       [{
-        stock_quantity: stock_quantity - amt
+        stock_quantity: amt
       },
       {
           id: prodID
